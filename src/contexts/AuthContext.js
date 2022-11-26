41fcff2f16b1b923fdb1408a695441e555cd3858
Paulo-Junio <paulofernandesbraga@gmail.com>
2022-11-26 11:15:38 -0300

    SignIn-SingUp-front

---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,15 @@
+import { useState, createContext } from 'react';
+
+
+export const AuthContext = createContext();
+
+export default function AuthProvider({ children }) {
+
+  const [token, setToken] = useState("");
+
+  return (
+    <AuthContext.Provider value={{ token, setToken }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
